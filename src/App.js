71b8216a004a4d8c099ec1af644df4ac1b938302
AmandaTodero <amanda.todero032@gmail.com>
2024-01-13 13:15:21 -0500

    Added Search Form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <div className="App-footer">
+          <footer>
+            <a
+              href="https://github.com/AmandaTodero3/dictionary-project"
+              target="_blank"
+            >
+              Open-sourced
+            </a>{" "}
+            code by Amanda Todero
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
